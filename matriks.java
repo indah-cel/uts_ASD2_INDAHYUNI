6cbe060a5367c7830f6efa80600170ab3110b2cd
indah-cel <72773127+indah-cel@users.noreply.github.com>
2021-04-20 11:37:48 +0700

    Add files via upload

---
+++ matriks.java
@@ -0,0 +1,44 @@
+
+package asd.uts;
+
+import java.util.Scanner;
+
+public class matriks {
+    public static void main(String[] args){
+        int i, j, m, n;
+        int matriks[][]= new int [10] [10];
+        int transpose [][]= new int [10][10];
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukan jumlah baris matriks: ");
+        m = scan.nextInt();
+        System.out.print("Masukan jumlah kolom baris matriks: ");
+        n = scan.nextInt();
+        System.out.print("Masukan elemen matriks: ");
+        for(i = 0; i< m; i++){
+          for(j= 0; j< n; j++){
+            matriks[i][j] = scan.nextInt();
+          }
+        }
+       System.out.println("Hasil matriks: ");
+       for(i = 0; i <m; i++){
+           for(j = 0; j< n; j++){
+               System.out.print(matriks[i][j]+ "/t");
+           }
+           System.out.println();
+       }
+       for(i = 0; i <m ; i++){
+           for(j = 0; j<n; j++){
+               transpose[j][i]=matriks[i][j];
+           }
+       }
+       System.out.println("Hasil transpose matriks: ");
+       for (i =0; i <n; i++){
+           for(j = 0; j< m; j++){
+        System.out.print(transpose[i][j] + "/t");
+    }
+           System.out.println();
+       }
+    }
+}
+    
+
